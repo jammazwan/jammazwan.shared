042127f6ffe0b532fe01b9416aa2c4f2f8c60859
petecarapetyan <pete@couldbe.net>
2016-06-08 04:54:40 -0500

    Added files for xam - xao

---
+++ src/main/java/jammazwan/entity/Manager.java
@@ -0,0 +1,69 @@
+package jammazwan.entity;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Manager {
+	@XmlAttribute
+	private String city;
+	@XmlAttribute
+	private String company;
+	@XmlAttribute
+	private String firstName;
+	@XmlAttribute
+	private String surname;
+
+	public Manager() {
+		super();
+	}
+
+	public Manager(String city, String company, String firstName, String surname) {
+		super();
+		this.city = city;
+		this.company = company;
+		this.firstName = firstName;
+		this.surname = surname;
+	}
+
+	public String toString() {
+		return "City: " + city + " Company: " + company + " Name " + firstName + " " + surname;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+}
--- src/main/java/jammazwan/util/HoldContextOpenUntilDone.java
+++ src/main/java/jammazwan/util/HoldContextOpenUntilDone.java
@@ -10,13 +10,12 @@ import org.apache.camel.model.ModelCamelContext;
 public class HoldContextOpenUntilDone {
 	public static void go(ModelCamelContext context) {
 		try {
-			System.err.println("Starting\n");
 			for (int i = 0; i < 30; i++) {
 				System.err.print(" " + i);
 				Thread.sleep(1000);
 				if (context.getInflightRepository().size() == 0) {
-					System.err.print(" Looks like we're done");
-					Thread.sleep(2000);
+					System.err.println("\nHoldContextOpenUntilDone is complete\n");
+					Thread.sleep(1000);
 					if (context.getInflightRepository().size() == 0) {
 						break;
 					}
